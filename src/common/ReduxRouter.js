25b8b24d2c4c289e0f3f6bd9f85c5f1af6c6135d
Florian Eibeck <florian.eibeck@mayflower.de>
2017-09-21 11:08:32 +0200

    The redux-first-router now handles history internally
    This is a breaking change in redux-first-router and the instance
    creation has to be changed.

--- src/common/ReduxRouter.js
+++ src/common/ReduxRouter.js
@@ -1,15 +1,11 @@
 import { connectRoutes } from "redux-first-router"
 
-/* eslint-disable import/no-commonjs */
-const createHistory = process.env.TARGET === "web" ?
-  require("history/createBrowserHistory").default :
-  require("history/createMemoryHistory").default
-
+// eslint-disable-next-line max-params
 export function createReduxRouter(routes, path = null, config = {}) {
   // match initial route to express path
-  const history = path ? createHistory({
-    initialEntries: [ path ]
-  }) : createHistory()
+  if (path) {
+    config.initialEntries = [ path ]
+  }
 
-  return connectRoutes(history, routes)
+  return connectRoutes(routes, config)
 }
